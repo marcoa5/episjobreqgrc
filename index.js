c8121d1b28450dd48d0fae5e734a83a1c1aed534
marcoa5 <marco.arato@gmail.com>
2022-09-21 13:42:07 +0200

    k

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ const porta = process.env.PORT || 3000
 const ver = require('./package.json').version
 var serviceAccount = require('./key.json')
 const admin = require('firebase-admin')
+const Handlebars = require("handlebars");
+const fs = require('fs')
+var html_to_pdf = require('html-pdf-node')
+const firebase = require('firebase/app')
+require('firebase/storage')
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'episerjob@gmail.com',
+      pass: 'xvesvmaufsunnzvr' 
+    }
+  });
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA9OHPbSNKBJUE7DqLAopJkfMMICo8hkHw",
+    authDomain: "episjobadmingrc.firebaseapp.com",
+    databaseURL: "https://episjobadmingrc-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "episjobadmingrc",
+    storageBucket: "episjobadmingrc.appspot.com",
+    messagingSenderId: "918912403305",
+    appId: "1:918912403305:web:4346393bf9409facc91ff8",
+    measurementId: "G-R54FWQY8XB"
+    })
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -94,6 +120,32 @@ app.get('/delete',function(req,res){
     })
 })
 
+app.all('/sjPdf', function(req,res){
+    var a = fs.readFileSync('template/template.html','utf8')
+    var templ = Handlebars.compile(a)
+    let options = {width: '21cm', height: '29.7cm'};
+    let file = {content: templ(req.body)}
+    html_to_pdf.generatePdf(file,options).then((d)=>{
+        res.end(d)
+    })
+})
+
+app.all('/sendSJNew', cors(), function(req,res){
+    let g = req.body
+    createPDF(g).then(urlPdf=>{
+        g.info.urlPdf = urlPdf
+        admin.auth().getUser(g.userId).then(user=>{
+            g.info.ccAuth = user.email
+            transporter.sendMail(createMailOptionsNew(g), (error, info)=>{
+                if (error) res.status(300).send(error)
+                if(info) {
+                    res.status(200).json({mailResult: info})             
+                }
+            })   
+        })
+    })
+})
+
 app.all('/', function(req, res,next) {
     const welc = `
     <div style="position: fixed; top:0;left:0;display:flex; justify-content: center; align-items: center; width:100%; height:100%; background-color: rgb(66, 85, 99)">
@@ -117,3 +169,47 @@ app.all('*', function(req, res,next) {
 app.listen(porta, ()=>{
     console.log(`Running on port:${porta}`)
 });
+
+
+
+
+// FUNCITONS
+
+function createPDF(b){
+    return new Promise((res,rej)=>{
+        var a = fs.readFileSync('template/template.html','utf8')
+        var templ = Handlebars.compile(a)
+        let options = {width: '21cm', height: '29.7cm'};
+        let file = {content: templ(b)}
+        html_to_pdf.generatePdf(file,options).then((d)=>{
+            let ref = firebase.default.storage().ref(b.author + '/' + b.info.fileName + '.pdf')
+            ref.put(Uint8Array.from(Buffer.from(d)).buffer, {contentType: 'application/pdf'})
+            .then(()=>{
+                ref.getDownloadURL().then(url=>{
+                    res(url)
+                })
+            })
+        })
+    })
+}
+
+function createMailOptionsNew(a){
+    let cc=[]
+    cc.push('dimitris.nikolakopoulos.epiroc.com')
+    if(!cc.includes(a.info.ccAuth)) cc.push(a.info.ccAuth)
+    let tech= a.author
+    const mailOptionsNew = {
+            from: `${a.author} - Epiroc Service <episerjob@gmail.com>`,
+            replyTo: 'dimitris.nikolakopoulos@epiroc.com',
+            to: 'marco.arato@epiroc.com',//a.elencomail,
+            cc: a.info.cc? cc.join(';'):'',
+            subject: a.info.subject,
+            text: `Please find attached Service Job by Epiroc techinician Mr. ${tech}.\nThank you for completing the survey.\n\n\nSurvey Results:\n\nPlanning: ${a.rissondaggio.split('')[0]}\nParts Delivery: ${a.rissondaggio.split('')[1]}\nExecution: ${a.rissondaggio.split('')[2]}`,
+            attachments: {
+                filename: a.info.fileName + '.pdf',
+                path: a.info.urlPdf
+            }
+        }
+
+    return (mailOptionsNew)
+}
